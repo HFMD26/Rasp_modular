51ac2d7ca6234143ed3679d02854df46777905a9
HFMD26 <144084434+HFMD26@users.noreply.github.com>
2026-05-07 15:31:17 -0600

    Create test_linea.launch.py

---
+++ src/proyecto_modula/launch/test_linea.launch.py
@@ -0,0 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('proyecto_modula') # Cambia por tu nombre de paquete
+    map_file = os.path.join(pkg_dir, 'maps', 'mapa_proyecto.yaml')
+
+    return LaunchDescription([
+        # 1. Servidor del Mapa
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            parameters=[{'yaml_filename': map_file}, {'use_sim_time': False}]
+        ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_map',
+            parameters=[{'autostart': True}, {'node_names': ['map_server']}]
+        ),
+
+        # 2. Tu script de movimiento
+        Node(
+            package='proyecto_modula',
+            executable='linea_recta.py', # Asegúrate que el archivo tenga permisos de ejecución
+            output='screen'
+        )
+    ])
